6dc7b807606a13ed872c9d7259f6a3601744d6e9
unknown <davidpaulminton@gmail.com>
2014-01-07 17:41:11 -0500

    fixed topcount partial

--- app/views/partial/_topcount.blade.php
+++ app/views/partial/_topcount.blade.php
@@ -3,7 +3,7 @@
 	<h4>Most Visited</h4>
 		<ul>
 		@foreach($topcount as $site)
-			<li>{{ HTML::link($site->url, "www.urlshortener.eu1.frbit.net/$site->shortened") . " - " . $site->count }}</li>
+			<li>{{ HTML::link($site, $site->url) . " - " . $site->count }}</li>
 		@endforeach
 		</ul>
 	</div>
